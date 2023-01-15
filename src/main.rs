2511ce9353ad80758e2774f272d07ce09fcd5957
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2023-01-15 22:00:59 +0900

    Test of cursive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use cursive::{
+    event::Key,
+    menu,
+    views::{Dialog}
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = cursive::default();
+
+    siv.menubar()
+        .add_subtree(
+            "Help",
+            menu::Tree::new()
+                .subtree(
+                    "Help",
+                    menu::Tree::new()
+                        .leaf("General", |s| s.add_layer(Dialog::info("Help message!"))),
+                )
+                .leaf("About", |s| s.add_layer(Dialog::info("Cursive v0.0.0"))),
+        )
+        .add_delimiter()
+        .add_leaf("Quit", |s| s.quit());
+
+    siv.set_window_title("A");
+
+    siv.add_global_callback(Key::Esc, |s| s.select_menubar());
+
+    siv.run();
 }

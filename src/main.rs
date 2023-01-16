5ed26b8c217b2d70aee14cd04fc84e2d0702a654
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2023-01-16 09:10:06 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,67 @@
 use cursive::{
-    event::Key,
+    align::HAlign,
+    event::{EventResult, Key},
     menu,
-    views::{Dialog}
+    view::{scroll::Scroller, Nameable, Resizable, Scrollable},
+    views::{Dialog, OnEventView, Panel, SelectView, TextView},
+    With,
 };
 
 fn main() {
     let mut siv = cursive::default();
 
-    siv.menubar()
-        .add_subtree(
-            "Help",
-            menu::Tree::new()
-                .subtree(
-                    "Help",
-                    menu::Tree::new()
-                        .leaf("General", |s| s.add_layer(Dialog::info("Help message!"))),
-                )
-                .leaf("About", |s| s.add_layer(Dialog::info("Cursive v0.0.0"))),
-        )
-        .add_delimiter()
-        .add_leaf("Quit", |s| s.quit());
+    siv.set_autohide_menu(false);
+    siv.set_window_title(format!("KMCL - v{}", env!("CARGO_PKG_VERSION")));
 
-    siv.set_window_title("A");
+    siv.menubar().add_subtree(
+        "File",
+        menu::Tree::new()
+            .subtree(
+                "Instances",
+                menu::Tree::new()
+                    .leaf("Add Instance", |s| {
+                        s.add_layer(Dialog::info("Add instance!"))
+                    })
+                    .leaf("Edit Instance", |s| {
+                        s.add_layer(Dialog::info("Edit instance!"))
+                    }),
+            )
+            .leaf("About", |s| {
+                s.add_layer(Dialog::around(Panel::new(
+                  TextView::new("aAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAAaAAAAAAAAAAAAAAAAAAA")
+                      .scrollable()
+                      .wrap_with(OnEventView::new)
+                      .on_pre_event_inner(Key::PageUp, |v, _| {
+                          let scroller = v.get_scroller_mut();
+                          if scroller.can_scroll_up() {
+                              scroller.scroll_up(
+                                  scroller.last_outer_size().y.saturating_sub(1),
+                              );
+                          }
+                          Some(EventResult::Consumed(None))
+                      })
+                      .on_pre_event_inner(Key::PageDown, |v, _| {
+                          let scroller = v.get_scroller_mut();
+                          if scroller.can_scroll_down() {
+                              scroller.scroll_down(
+                                  scroller.last_outer_size().y.saturating_sub(1),
+                              );
+                          }
+                          Some(EventResult::Consumed(None))
+                      })
+                      .fixed_size((s.screen_size().x / 2, s.screen_size().y / 2)),
+              ))
+              .title("About KMCL")
+              .h_align(HAlign::Center)
+              .button("Close", |s| {
+                s.pop_layer();
+              })
+            )
+            })
+            .leaf("Quit", |s| s.quit()),
+    );
+
+    siv.add_layer(Dialog::text("Hello!").content(SelectView::<String>::new().with_name("select")));
 
     siv.add_global_callback(Key::Esc, |s| s.select_menubar());
 

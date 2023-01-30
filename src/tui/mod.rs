8fbd6025ef74fb14340b7b7fe29bd3d9a1d8441f
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2023-01-30 16:15:09 +0900

    Remove new line

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -21,7 +21,6 @@ pub fn run() {
 
     let app_version = env!("CARGO_PKG_VERSION");
 
-
     match instance::get_instance("test123") {
         Ok(inst) => {
             println!("{:?}", inst);
@@ -47,26 +46,26 @@ pub fn run() {
             t!("menu.instance.title"),
             menu::Tree::new().leaf(t!("menu.instance.view_folder"), |s| {
                 match instance::get_instance_dir() {
-                        Ok(instance) => {
-                            if let Err(e) = open::that(instance) {
-                                s.add_layer(
-                                    Dialog::around(TextView::new(format!("Error: {}", e)))
-                                        .title("Failed to open instance directory.")
-                                    .dismiss_button(t!("dialog.button.close")),
-                                );
-                            };
-                        }
-                        Err(e) => {
-                        println!("{}", 0x07);
+                    Ok(instance) => {
+                        if let Err(e) = open::that(instance) {
                             s.add_layer(
-                                Dialog::around(TextView::new(format!(
-                                    "Failed to get instance directory: {}",
-                                    e
-                                )))
+                                Dialog::around(TextView::new(format!("Error: {}", e)))
+                                    .title("Failed to open instance directory.")
+                                    .dismiss_button(t!("dialog.button.close")),
+                            );
+                        };
+                    }
+                    Err(e) => {
+                        println!("{}", 0x07);
+                        s.add_layer(
+                            Dialog::around(TextView::new(format!(
+                                "Failed to get instance directory: {}",
+                                e
+                            )))
                             .title(t!("dialog.error.title"))
                             .dismiss_button(t!("dialog.button.close")),
-                            );
-                        }
+                        );
+                    }
                 }
             }),
         );
@@ -85,19 +84,28 @@ pub fn run() {
         view.add_item_str("test123");
     });
 
+    siv.call_on_name("instance_list", |view: &mut SelectView<String>| {
+        view.add_item_str("aaaa");
+    });
+
+    siv.add_global_callback(event::Key::Esc, |s| s.select_menubar());
+
+    siv.run();
+}
+
 fn on_submit(s: &mut Cursive, name: &str) {
     match instance::get_instance(name) {
-                        Ok(instance) => s.add_layer(
-                            Dialog::around(TextView::new(formatdoc!(
-                                "
-                                Name: {}
-                                Version: {}",
-                                instance.name,
-                                instance.version
-                            )))
+        Ok(instance) => s.add_layer(
+            Dialog::around(TextView::new(formatdoc!(
+                "
+                Name: {}
+                Version: {}",
+                instance.name,
+                instance.version
+            )))
             .title(t!("dialog.instance_info.title"))
             .dismiss_button(t!("dialog.button.close")),
-                        ),
+        ),
         Err(e) => {
             s.add_layer(
                 Dialog::around(TextView::new(formatdoc!(
@@ -105,12 +113,12 @@ fn on_submit(s: &mut Cursive, name: &str) {
                 Failed to get instance {}:
                 {}",
                     name,
-                                e.to_string()
-                            )))
+                    e.to_string()
+                )))
                 .title(t!("dialog.error.title"))
                 .dismiss_button(t!("dialog.button.ok")),
             );
             println!("{}", BEL);
         }
-                    };
+    };
 }
